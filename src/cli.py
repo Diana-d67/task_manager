b3a1c2ec26fd52264aa33bcfa1a06da92626c728
KOMP-8 <dustuzina39@gmail.com>
2026-03-27 10:18:35 +1000

    сделала cli

--- src/cli.py
+++ src/cli.py
@@ -21,8 +21,40 @@ def run_cli():
 
         if choice == "1":
             title = input("Введите задачу: ")
-            tasks = add_task(title, tasks)
+            tasks = add_task(tasks, title)
 
         elif choice == "2":
             for task in tasks:
                 print(task)
+
+
+        elif choice == "3":
+            status = input("Введите статус: ")
+            if validate_status(status):
+                result = get_tasks_by_status(tasks, status)
+                for task in result:
+                    print(task)
+            else:
+                print("Ошибка статуса!")
+
+        elif choice == "4":
+            order = input("По убыанию? (y\n): ")
+            reverse = order == "y"
+            tasks = sort_tasks(tasks, reverse)
+            print("Отсортировано")
+
+        elif choice == "5":
+            save_tasks(tasks, FILENAME)
+            print("Сохоанено")
+
+        elif choice == "0":
+            save = input("Сохранить перед выходом? (y\n): ")
+            if save == "y":
+                save_tasks(tasks, FILENAME)
+            break
+
+        else:
+            print("Ошибка ввода")
+
+if __name__ == "__main__":
+    run_cli()
